03ce39831c36e33194c79677b317c11668731d7b
Siddhant Hullur <51142971+sidhullur12@users.noreply.github.com>
2022-09-02 18:20:23 -0700

    Initial Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Siddhant Hullur/Documents/server
+# Build directory: C:/Users/Siddhant Hullur/Documents/server/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
